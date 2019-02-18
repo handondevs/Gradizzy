e61328ae536a20c8ecfefccc550546ce529375c8
thantienduc <duc.than@mavs.uta.edu>
2019-02-17 20:42:43 -0500

    add student information

---
+++ test/test_extractStudentinfo.js
@@ -0,0 +1,3 @@
+const test = require('../userdata/demo/grd-scripts');
+
+test.extractStudentinfo('C:/Project/gradizzy/SampleHW1/Homework 1_pxd6630_attempt_2019-01-30-01-20-28.txt');
--- userdata/demo/grd-scripts.js
+++ userdata/demo/grd-scripts.js
@@ -43,12 +43,74 @@ const testAll = function() {
 
 }
 
+const readNext = function(string, delimiter){
+    var num = string.indexOf(delimiter);
+    var res = string.substring(1,num);
+    var re = string.substring(num);
+    return{ 
+        result: res,
+        remain: re,
+    }
+}
+
+const extractStudentinfo = function(path) {
+    const fs =require('fs')
+    fs.readFile(path, (err,data)=> {
+        if (err) throw err;
+        var string = data.toString();
+        //console.log(string);
+        delim=":";
+        var res=readNext(string,delim).result;
+        var re = readNext(string, delim).remain;
+        delim = "(";
+        var name = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim = ")";
+        var id = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim = ":";
+        var re = readNext(re,delim).remain;
+        delim ="\n";
+        var assignment = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim = ":";
+        var re = readNext(re,delim).remain;
+        delim ="\n";
+        var date = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim=":";
+        var status = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim =".";
+        var re = readNext(re,delim).remain;
+        delim=":";
+        var re = readNext(re,delim).remain;
+        delim =".";
+        var re = readNext(re,delim).remain;
+        delim =":";
+        var re = readNext(re,delim).remain;
+        delim="\n";
+        var re = readNext(re,delim).remain;
+        delim=":";
+        var re = readNext(re,delim).remain;
+        delim ="\n";
+        var origin_filename = readNext(re,delim).result;
+        var re = readNext(re,delim).remain;
+        delim =":";
+        var re  = readNext(re,delim).remain;
+        delim ="\n";
+        var filename  = readNext(re,delim).result;
+       console.log(name," ",id," ",assignment," ",date," ",origin_filename,"\n ",filename);
+    })
+}
+
 module.exports = {
     getTestPath,
     submissions,
-    getSubmissionReport,
+    //getSubmissionReport,
     getReport,
-    getAllReport,
+    //getAllReport,
     testSubmission,
     testAll,
+    extractStudentinfo,
 };
